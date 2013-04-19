6578d4eb1df5579b6cb42c512673a698a03208c8
SOnja <sonja@sonja-TOSHIBA-NB505.(none)>
2013-04-18 20:58:03 -0700

    I added the script that sends the log file to the parser one line at a time

---
+++ scripts/file_load.py
@@ -0,0 +1,27 @@
+#put me in base(?) core(?)/scripts
+import json
+import fileinput
+import requests
+
+class Loader(object):
+	def __init__(self):
+		self.name = "Ahmad"
+		self.api_key = "sdhufuserhfhrjf"
+		self.api = "a website"
+		self.machine_name = "Terminator"
+		self.results = []
+
+	def do_it():
+		for line in fileinput.input():
+			d = {"username": self.name, 
+				"api_key": self.api_key, 
+				"machine_name": self.machine_name, 
+				"line": line, 
+				"line_number": fileinput.filelineno()}
+			result = requests.post(self.api, data=json.dumps(d))
+			self.results.append(result)
+		fileinput.close()
+
+if __name__ == "__main__":
+	load = Loader()
+	load.do_it()
